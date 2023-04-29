a687b957330a16d25ddaed91a6db0650b8de1f32
UsefullyUseless90 <84840316+UsefullyUseless90@users.noreply.github.com>
2023-04-29 16:39:36 +0200

    Update AssessmentServiceImpl.java

--- MediscreenDiabetesAssessment/src/main/java/com/mediscreen/DiabetesAssessment/service/AssessmentServiceImpl.java
+++ MediscreenDiabetesAssessment/src/main/java/com/mediscreen/DiabetesAssessment/service/AssessmentServiceImpl.java
@@ -74,17 +74,13 @@ public class AssessmentServiceImpl implements AssessmentService {
             if(patientReportById.getAge() > 30){
                 patientReportById.setAssessment(assessAssignment.assignAssessThirtyPlus(triggerCount).toString());
             }
-            if (patientReportById.getPatient().getGender() == "male" || patientReportById.getPatient().getGender() == "female") {
-                if (patientReportById.getPatient().getGender().equals("male")) {
-                    if (patientReportById.getAge() < 30 || patientReportById.getPatient().equals(30)) {
-                        patientReportById.setAssessment(assessAssignment.assignAssessMale(triggerCount).toString());
-                    }
-                } else if (patientReportById.getPatient().getGender().equals("female")) {
-                    if (patientReportById.getAge() < 30 || patientReportById.getPatient().equals(30)) {
-                        patientReportById.setAssessment(assessAssignment.assignAssessFemale(triggerCount).toString());
-                    }
-                }
+        if (patientReportById.getAge() < 30 || patientReportById.getPatient().equals(30)) {
+            if (patientReportById.getPatient().getGender().equals("male")) {
+                patientReportById.setAssessment(assessAssignment.assignAssessMale(triggerCount));
+            } else {
+                patientReportById.setAssessment(assessAssignment.assignAssessFemale(triggerCount));
             }
+        }
         return patientReportById;
     }
 
@@ -114,19 +110,16 @@ public class AssessmentServiceImpl implements AssessmentService {
             }
         }
         if(patientReportByPatientName.getAge() > 30){
-            patientReportByPatientName.setAssessment(assessAssignment.assignAssessThirtyPlus(triggerCount).toString());
+            patientReportByPatientName.setAssessment(assessAssignment.assignAssessThirtyPlus(triggerCount));
         }
-        if (patientReportByPatientName.getPatient().getGender() == "male" || patientReportByPatientName.getPatient().getGender() == "female") {
+        if (patientReportByPatientName.getAge() < 30 || patientReportByPatientName.getPatient().equals(30)) {
             if (patientReportByPatientName.getPatient().getGender().equals("male")) {
-                if (patientReportByPatientName.getAge() < 30 || patientReportByPatientName.getPatient().equals(30)) {
-                    patientReportByPatientName.setAssessment(assessAssignment.assignAssessMale(triggerCount).toString());
-                }
-            } else if (patientReportByPatientName.getPatient().getGender().equals("female")) {
-                if (patientReportByPatientName.getAge() < 30 || patientReportByPatientName.getPatient().equals(30)) {
-                    patientReportByPatientName.setAssessment(assessAssignment.assignAssessFemale(triggerCount).toString());
-                }
+                patientReportByPatientName.setAssessment(assessAssignment.assignAssessMale(triggerCount));
+            }else{
+                patientReportByPatientName.setAssessment(assessAssignment.assignAssessFemale(triggerCount));
             }
         }
+
         return patientReportByPatientName;
         }
 
@@ -139,20 +132,6 @@ public class AssessmentServiceImpl implements AssessmentService {
 
             return report;
         }
-/*
-        public Map<String, Long> triggers(List<String> commentaries , List<String> triggers){
 
-        Map<String, Long> map = commentaries.stream().map(c -> c.split("\\b" + "\\s")).flatMap(w-> Arrays.stream(w).distinct()).filter(triggers::contains).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-        return map;
-        }
-/*
-        public Long countTriggers(List<String> commentaries) throws JsonProcessingException {
-        List<String> trigsToUpper = trigs.stream().map(String::toUpperCase).collect(Collectors.toList());
-        List<String> commentariesToUpper = commentaries.stream().map(String::toUpperCase).collect(Collectors.toList());
-        Map<String, List<String>> count1 = trigsToUpper.stream().flatMap(trigger -> trigsToUpper.stream().filter(commentariesToUpper::contains)).collect(Collectors.groupingBy(Function.identity()));
-        long count = count1.size();
-        return count;
-        }
-
- */
     }
+
